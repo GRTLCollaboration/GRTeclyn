ba8b44c4543ab7ec74efe11def6ca75f6d3cb7e5
Miren Radia <miren_radia@yahoo.co.uk>
2023-05-23 12:40:59 +0100

    Add unedited CCZ4 and dependent files from fdf5a7a

---
+++ Tests/CCZ4Test/CCZ4RHS-fdf5a7a.impl.hpp
@@ -0,0 +1,233 @@
+/* GRChombo
+ * Copyright 2012 The GRChombo collaboration.
+ * Please refer to LICENSE in GRChombo's root directory.
+ */
+
+#if !defined(CCZ4RHS_HPP_)
+#error "This file should only be included through CCZ4RHS.hpp"
+#endif
+
+#ifndef CCZ4RHS_IMPL_HPP_
+#define CCZ4RHS_IMPL_HPP_
+
+#include "DimensionDefinitions.hpp"
+#include "GRInterval.hpp"
+#include "VarsTools.hpp"
+
+template <class gauge_t, class deriv_t>
+inline CCZ4RHS<gauge_t, deriv_t>::CCZ4RHS(
+    CCZ4_params_t<typename gauge_t::params_t> a_params, double a_dx,
+    double a_sigma, int a_formulation, double a_cosmological_constant)
+    : m_params(a_params), m_gauge(a_params), m_sigma(a_sigma),
+      m_formulation(a_formulation),
+      m_cosmological_constant(a_cosmological_constant), m_deriv(a_dx)
+{
+    // A user who wants to use BSSN should also have damping paramters = 0
+    if (m_formulation == USE_BSSN)
+    {
+        if ((m_params.kappa1 != 0.) || (m_params.kappa2 != 0.) ||
+            (m_params.kappa3 != 0.))
+        {
+            amrex::Abort("BSSN formulation is selected - CCZ4 kappa values "
+                          "should be set to zero in params");
+        }
+    }
+    if (m_formulation > USE_BSSN)
+        amrex::Abort("The requested formulation is not supported");
+}
+
+template <class gauge_t, class deriv_t>
+template <class data_t>
+AMREX_GPU_DEVICE AMREX_FORCE_INLINE
+void CCZ4RHS<gauge_t, deriv_t>::compute(int i, int j, int k,
+                                        amrex::Array4<data_t> const& rhs,
+                                        amrex::Array4<data_t const> const& state) const
+{
+    const auto vars = load_vars<Vars>(state.cellData(i,j,k));
+    const auto d1 = m_deriv.template diff1<Vars>(i,j,k,state);
+    const auto d2 = m_deriv.template diff2<Diff2Vars>(i,j,k,state);
+    const auto advec =
+        m_deriv.template advection<Vars>(i,j,k,state,vars.shift);
+
+    Vars<data_t> rhs_vars;
+    rhs_equation(rhs_vars, vars, d1, d2, advec);
+
+    m_deriv.add_dissipation(i, j, k, rhs_vars, state, m_sigma);
+
+    store_vars(rhs.cellData(i,j,k), rhs_vars);
+}
+
+template <class gauge_t, class deriv_t>
+template <class data_t, template <typename> class vars_t,
+          template <typename> class diff2_vars_t>
+AMREX_GPU_DEVICE AMREX_FORCE_INLINE
+void CCZ4RHS<gauge_t, deriv_t>::rhs_equation(
+    vars_t<data_t> &rhs, const vars_t<data_t> &vars,
+    const vars_t<Tensor<1, data_t>> &d1,
+    const diff2_vars_t<Tensor<2, data_t>> &d2,
+    const vars_t<data_t> &advec) const
+{
+    using namespace TensorAlgebra;
+
+    auto h_UU = compute_inverse_sym(vars.h);
+    auto chris = compute_christoffel(d1.h, h_UU);
+
+    Tensor<1, data_t> Z_over_chi;
+    Tensor<1, data_t> Z;
+
+    if (m_formulation == USE_BSSN)
+    {
+        FOR(i) Z_over_chi[i] = 0.0;
+    }
+    else
+    {
+        FOR(i) Z_over_chi[i] = 0.5 * (vars.Gamma[i] - chris.contracted[i]);
+    }
+    FOR(i) Z[i] = vars.chi * Z_over_chi[i];
+
+    auto ricci =
+        CCZ4Geometry::compute_ricci_Z(vars, d1, d2, h_UU, chris, Z_over_chi);
+
+    data_t divshift = compute_trace(d1.shift);
+    data_t Z_dot_d1lapse = compute_dot_product(Z, d1.lapse);
+    data_t dlapse_dot_dchi = compute_dot_product(d1.lapse, d1.chi, h_UU);
+
+    Tensor<2, data_t> covdtilde2lapse;
+    Tensor<2, data_t> covd2lapse;
+    FOR(k, l)
+    {
+        covdtilde2lapse[k][l] = d2.lapse[k][l];
+        FOR(m) { covdtilde2lapse[k][l] -= chris.ULL[m][k][l] * d1.lapse[m]; }
+        covd2lapse[k][l] =
+            vars.chi * covdtilde2lapse[k][l] +
+            0.5 * (d1.lapse[k] * d1.chi[l] + d1.chi[k] * d1.lapse[l] -
+                   vars.h[k][l] * dlapse_dot_dchi);
+    }
+
+    data_t tr_covd2lapse = -(GR_SPACEDIM / 2.0) * dlapse_dot_dchi;
+    FOR(i)
+    {
+        tr_covd2lapse -= vars.chi * chris.contracted[i] * d1.lapse[i];
+        FOR(j)
+        {
+            tr_covd2lapse += h_UU[i][j] * (vars.chi * d2.lapse[i][j] +
+                                           d1.lapse[i] * d1.chi[j]);
+        }
+    }
+
+    Tensor<2, data_t> A_UU = raise_all(vars.A, h_UU);
+
+    // A^{ij} A_{ij}. - Note the abuse of the compute trace function.
+    data_t tr_A2 = compute_trace(vars.A, A_UU);
+    rhs.chi = advec.chi +
+              (2.0 / GR_SPACEDIM) * vars.chi * (vars.lapse * vars.K - divshift);
+    FOR(i, j)
+    {
+        rhs.h[i][j] = advec.h[i][j] - 2.0 * vars.lapse * vars.A[i][j] -
+                      (2.0 / GR_SPACEDIM) * vars.h[i][j] * divshift;
+        FOR(k)
+        {
+            rhs.h[i][j] +=
+                vars.h[k][i] * d1.shift[k][j] + vars.h[k][j] * d1.shift[k][i];
+        }
+    }
+
+    Tensor<2, data_t> Adot_TF;
+    FOR(i, j)
+    {
+        Adot_TF[i][j] =
+            -covd2lapse[i][j] + vars.chi * vars.lapse * ricci.LL[i][j];
+    }
+    make_trace_free(Adot_TF, vars.h, h_UU);
+
+    FOR(i, j)
+    {
+        rhs.A[i][j] = advec.A[i][j] + Adot_TF[i][j] +
+                      vars.A[i][j] * (vars.lapse * (vars.K - 2 * vars.Theta) -
+                                      (2.0 / GR_SPACEDIM) * divshift);
+        FOR(k)
+        {
+            rhs.A[i][j] +=
+                vars.A[k][i] * d1.shift[k][j] + vars.A[k][j] * d1.shift[k][i];
+            FOR(l)
+            {
+                rhs.A[i][j] -=
+                    2 * vars.lapse * h_UU[k][l] * vars.A[i][k] * vars.A[l][j];
+            }
+        }
+    }
+
+    data_t kappa1_times_lapse;
+    if (m_params.covariantZ4)
+        kappa1_times_lapse = m_params.kappa1;
+    else
+        kappa1_times_lapse = m_params.kappa1 * vars.lapse;
+
+    if (m_formulation == USE_BSSN)
+    {
+        rhs.Theta = 0; // ensure the Theta of CCZ4 remains at zero
+        // Use hamiltonian constraint to remove ricci.scalar for BSSN update
+        rhs.K = advec.K + vars.lapse * (tr_A2 + vars.K * vars.K / GR_SPACEDIM) -
+                tr_covd2lapse;
+        rhs.K += -2 * vars.lapse * m_cosmological_constant / (GR_SPACEDIM - 1.);
+    }
+    else
+    {
+        rhs.Theta =
+            advec.Theta +
+            0.5 * vars.lapse *
+                (ricci.scalar - tr_A2 +
+                 ((GR_SPACEDIM - 1.0) / (double)GR_SPACEDIM) * vars.K * vars.K -
+                 2 * vars.Theta * vars.K) -
+            0.5 * vars.Theta * kappa1_times_lapse *
+                ((GR_SPACEDIM + 1) + m_params.kappa2 * (GR_SPACEDIM - 1)) -
+            Z_dot_d1lapse;
+
+        rhs.Theta += -vars.lapse * m_cosmological_constant;
+        rhs.K =
+            advec.K +
+            vars.lapse * (ricci.scalar + vars.K * (vars.K - 2 * vars.Theta)) -
+            kappa1_times_lapse * GR_SPACEDIM * (1 + m_params.kappa2) *
+                vars.Theta -
+            tr_covd2lapse;
+        rhs.K += -2 * vars.lapse * GR_SPACEDIM / (GR_SPACEDIM - 1.) *
+                 m_cosmological_constant;
+    }
+
+    Tensor<1, data_t> Gammadot;
+    FOR(i)
+    {
+        Gammadot[i] = (2.0 / GR_SPACEDIM) *
+                          (divshift * (chris.contracted[i] +
+                                       2 * m_params.kappa3 * Z_over_chi[i]) -
+                           2 * vars.lapse * vars.K * Z_over_chi[i]) -
+                      2 * kappa1_times_lapse * Z_over_chi[i];
+        FOR(j)
+        {
+            Gammadot[i] +=
+                2 * h_UU[i][j] *
+                    (vars.lapse * d1.Theta[j] - vars.Theta * d1.lapse[j]) -
+                2 * A_UU[i][j] * d1.lapse[j] -
+                vars.lapse * ((2 * (GR_SPACEDIM - 1.0) / (double)GR_SPACEDIM) *
+                                  h_UU[i][j] * d1.K[j] +
+                              GR_SPACEDIM * A_UU[i][j] * d1.chi[j] / vars.chi) -
+                (chris.contracted[j] + 2 * m_params.kappa3 * Z_over_chi[j]) *
+                    d1.shift[i][j];
+
+            FOR(k)
+            {
+                Gammadot[i] +=
+                    2 * vars.lapse * chris.ULL[i][j][k] * A_UU[j][k] +
+                    h_UU[j][k] * d2.shift[i][j][k] +
+                    ((GR_SPACEDIM - 2.0) / (double)GR_SPACEDIM) * h_UU[i][j] *
+                        d2.shift[k][j][k];
+            }
+        }
+    }
+
+    FOR(i) { rhs.Gamma[i] = advec.Gamma[i] + Gammadot[i]; }
+
+    m_gauge.rhs_gauge(rhs, vars, d1, d2, advec);
+}
+
+#endif /* CCZ4RHS_IMPL_HPP_ */
---
+++ Tests/CCZ4Test/FourthOrderDerivatives-fdf5a7a.hpp
@@ -0,0 +1,244 @@
+/* GRChombo
+ * Copyright 2012 The GRChombo collaboration.
+ * Please refer to LICENSE in GRChombo's root directory.
+ */
+
+#ifndef FOURTHORDERDERIVATIVES_HPP_
+#define FOURTHORDERDERIVATIVES_HPP_
+
+#include "Cell.hpp"
+#include "DimensionDefinitions.hpp"
+#include "simd.hpp"
+#include "Tensor.hpp"
+#include "UserVariables.hpp"
+#include <array>
+
+class FourthOrderDerivatives
+{
+  public:
+    const double m_dx;
+
+  private:
+    const double m_one_over_dx;
+    const double m_one_over_dx2;
+
+  public:
+    FourthOrderDerivatives(double dx)
+        : m_dx(dx), m_one_over_dx(1 / dx), m_one_over_dx2(1 / (dx * dx))
+    {
+    }
+
+    template <class data_t>
+    AMREX_GPU_DEVICE AMREX_FORCE_INLINE
+    data_t diff1(const double *in_ptr, const int idx, const int stride) const
+    {
+        auto in = SIMDIFY<data_t>(in_ptr);
+
+        data_t weight_far = 8.33333333333333333333e-2;
+        data_t weight_near = 6.66666666666666666667e-1;
+
+        // NOTE: if you have been sent here by the debugger because of
+        // EXC_BAD_ACCESS  or something similar you might be trying to take
+        // derivatives without ghost points.
+        return (weight_far * in[idx - 2 * stride] -
+                weight_near * in[idx - stride] +
+                weight_near * in[idx + stride] -
+                weight_far * in[idx + 2 * stride]) *
+               m_one_over_dx;
+    }
+
+    /// Calculates all first derivatives and returns as variable type specified
+    /// by the template parameter
+    template <template <typename> class vars_t, class data_t>
+    AMREX_GPU_DEVICE AMREX_FORCE_INLINE
+    auto diff1(int i, int j, int k, const amrex::Array4<data_t const> &state) const
+    {
+        vars_t<Tensor<1, data_t>> d1;
+        auto p = state.ptr(i,j,k);
+        d1.enum_mapping([&](const int &ivar, Tensor<1, data_t> &var) {
+            AMREX_D_TERM(var[0] = diff1<data_t>(p+ivar*state.nstride, 0, 1);,
+                         var[1] = diff1<data_t>(p+ivar*state.nstride, 0, static_cast<int>(state.jstride));,
+                         var[2] = diff1<data_t>(p+ivar*state.nstride, 0, static_cast<int>(state.kstride)));
+        });
+        return d1;
+    }
+
+    template <class data_t>
+    AMREX_GPU_DEVICE AMREX_FORCE_INLINE
+    data_t diff2(const double *in_ptr, const int idx, const int stride) const
+    {
+        auto in = SIMDIFY<data_t>(in_ptr);
+
+        data_t weight_far = 8.33333333333333333333e-2;
+        data_t weight_near = 1.33333333333333333333e+0;
+        data_t weight_local = 2.50000000000000000000e+0;
+
+        return (-weight_far * in[idx - 2 * stride] +
+                weight_near * in[idx - stride] - weight_local * in[idx] +
+                weight_near * in[idx + stride] -
+                weight_far * in[idx + 2 * stride]) *
+               m_one_over_dx2;
+    }
+
+    template <class data_t>
+    AMREX_GPU_DEVICE AMREX_FORCE_INLINE
+    data_t mixed_diff2(const double *in_ptr, const int idx,
+                       const int stride1, const int stride2) const
+    {
+        auto in = SIMDIFY<data_t>(in_ptr);
+
+        data_t weight_far_far = 6.94444444444444444444e-3;
+        data_t weight_near_far = 5.55555555555555555556e-2;
+        data_t weight_near_near = 4.44444444444444444444e-1;
+
+        return (weight_far_far * in[idx - 2 * stride1 - 2 * stride2] -
+                weight_near_far * in[idx - 2 * stride1 - stride2] +
+                weight_near_far * in[idx - 2 * stride1 + stride2] -
+                weight_far_far * in[idx - 2 * stride1 + 2 * stride2]
+
+                - weight_near_far * in[idx - stride1 - 2 * stride2] +
+                weight_near_near * in[idx - stride1 - stride2] -
+                weight_near_near * in[idx - stride1 + stride2] +
+                weight_near_far * in[idx - stride1 + 2 * stride2]
+
+                + weight_near_far * in[idx + stride1 - 2 * stride2] -
+                weight_near_near * in[idx + stride1 - stride2] +
+                weight_near_near * in[idx + stride1 + stride2] -
+                weight_near_far * in[idx + stride1 + 2 * stride2]
+
+                - weight_far_far * in[idx + 2 * stride1 - 2 * stride2] +
+                weight_near_far * in[idx + 2 * stride1 - stride2] -
+                weight_near_far * in[idx + 2 * stride1 + stride2] +
+                weight_far_far * in[idx + 2 * stride1 + 2 * stride2]) *
+               m_one_over_dx2;
+    }
+
+    /// Calculates all second derivatives and returns as variable type specified
+    /// by the template parameter
+    template <template <typename> class vars_t, class data_t>
+    AMREX_GPU_DEVICE AMREX_FORCE_INLINE
+    auto diff2(int i, int j, int k, amrex::Array4<data_t const> const& state) const
+    {
+        vars_t<Tensor<2, data_t>> d2;
+        auto p = state.ptr(i,j,k);
+        amrex::GpuArray<int,AMREX_SPACEDIM> strides{1,static_cast<int>(state.jstride),static_cast<int>(state.kstride)};
+        d2.enum_mapping([&](const int &ivar, Tensor<2, data_t> &var) {
+            auto pvar = p+ivar*state.nstride;
+            FOR(dir1) // First calculate the repeated derivatives
+            {
+                var[dir1][dir1] = diff2<data_t>(pvar, 0, strides[dir1]);
+                for (int dir2 = 0; dir2 < dir1; ++dir2)
+                {
+                    auto tmp = mixed_diff2<data_t>(pvar, 0, strides[dir1], strides[dir2]);
+                    var[dir1][dir2] = tmp;
+                    var[dir2][dir1] = tmp;
+                }
+            }
+        });
+        return d2;
+    }
+
+  protected: // Let's keep this protected ... we may want to change the
+             // advection calculation
+    template <class data_t, class mask_t>
+    AMREX_GPU_DEVICE AMREX_FORCE_INLINE
+    data_t advection_term(const double *in_ptr, const int idx,
+                          const data_t &vec_comp,
+                          const int stride,
+                          const mask_t shift_positive) const
+    {
+        const auto in = SIMDIFY<data_t>(in_ptr);
+        const data_t in_left = in[idx - stride];
+        const data_t in_centre = in[idx];
+        const data_t in_right = in[idx + stride];
+
+        data_t weight_0 = -2.50000000000000000000e-1;
+        data_t weight_1 = -8.33333333333333333333e-1;
+        data_t weight_2 = +1.50000000000000000000e+0;
+        data_t weight_3 = -5.00000000000000000000e-1;
+        data_t weight_4 = +8.33333333333333333333e-2;
+
+        data_t upwind;
+        upwind = vec_comp *
+                 (weight_0 * in_left + weight_1 * in_centre +
+                  weight_2 * in_right + weight_3 * in[idx + 2 * stride] +
+                  weight_4 * in[idx + 3 * stride]) *
+                 m_one_over_dx;
+
+        data_t downwind;
+        downwind = vec_comp *
+                   (-weight_4 * in[idx - 3 * stride] -
+                    weight_3 * in[idx - 2 * stride] - weight_2 * in_left -
+                    weight_1 * in_centre - weight_0 * in_right) *
+                   m_one_over_dx;
+
+        return simd_conditional(shift_positive, upwind, downwind);
+    }
+
+  public:
+
+    /// Calculates all second derivatives and returns as variable type specified
+    /// by the template parameter
+    template <template <typename> class vars_t, class data_t>
+    AMREX_GPU_DEVICE AMREX_FORCE_INLINE
+    auto advection(int i, int j, int k,
+                   amrex::Array4<data_t const> const& state,
+                   const Tensor<1, data_t> &vector) const
+    {
+        vars_t<data_t> advec;
+        auto p = state.ptr(i,j,k);
+        amrex::GpuArray<int,AMREX_SPACEDIM> strides{1,static_cast<int>(state.jstride),static_cast<int>(state.kstride)};
+        advec.enum_mapping([&](const int &ivar, data_t &var) {
+            var = 0.;
+            auto pvar = p+ivar*state.nstride;
+            FOR(dir)
+            {
+                const auto shift_positive = simd_compare_gt(vector[dir], 0.0);
+                var += advection_term(pvar, 0, vector[dir], strides[dir],
+                                      shift_positive);
+            }
+        });
+        return advec;
+    }
+
+    template <class data_t>
+    AMREX_GPU_DEVICE AMREX_FORCE_INLINE
+    data_t dissipation_term(const double *in_ptr, const int idx,
+                            const int stride) const
+    {
+        const auto in = SIMDIFY<data_t>(in_ptr);
+        data_t weight_vfar = 1.56250e-2;
+        data_t weight_far = 9.37500e-2;
+        data_t weight_near = 2.34375e-1;
+        data_t weight_local = 3.12500e-1;
+
+        return (weight_vfar * in[idx - 3 * stride] -
+                weight_far * in[idx - 2 * stride] +
+                weight_near * in[idx - stride] - weight_local * in[idx] +
+                weight_near * in[idx + stride] -
+                weight_far * in[idx + 2 * stride] +
+                weight_vfar * in[idx + 3 * stride]) *
+               m_one_over_dx;
+    }
+
+    template <class data_t, template <typename> class vars_t>
+    AMREX_GPU_DEVICE AMREX_FORCE_INLINE
+    void add_dissipation(int i, int j, int k,
+                         vars_t<data_t> &vars,
+                         amrex::Array4<data_t const> const& state,
+                         const double factor) const
+    {
+        auto p = state.ptr(i,j,k);
+        amrex::GpuArray<int,AMREX_SPACEDIM> strides{1,static_cast<int>(state.jstride),static_cast<int>(state.kstride)};
+        vars.enum_mapping([&](const int &ivar, data_t &var) {
+            FOR(dir)
+            {
+                const auto stride = strides[dir];
+                var +=
+                    factor * dissipation_term<data_t>(p+ivar*state.nstride, 0, stride);
+            }
+        });
+    }
+};
+
+#endif /* FOURTHORDERDERIVATIVES_HPP_ */
