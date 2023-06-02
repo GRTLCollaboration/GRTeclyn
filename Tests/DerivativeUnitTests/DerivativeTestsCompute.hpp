84ec3f940f1851b10fb8e1c0dc7d983a8d45420b
Miren Radia <miren_radia@yahoo.co.uk>
2023-06-02 14:59:36 +0100

    Add Derivatives unit test
    Make test smaller than the GRChombo equivalent (only 32x32 rather than
    512x512) to reduce number of Catch2 assertions.
    
    Sixth order derivatives are not yet implemented so this section of test
    is commented out.

--- Tests/DerivativeUnitTests/DerivativeTestsCompute.hpp
+++ Tests/DerivativeUnitTests/DerivativeTestsCompute.hpp
@@ -6,15 +6,31 @@
 #ifndef DERIVATIVETESTSCOMPUTE_HPP_
 #define DERIVATIVETESTSCOMPUTE_HPP_
 
+#include "Cell.hpp"
 #include "VarsTools.hpp"
+
+enum
+{
+    c_d1,
+    c_d2,
+    c_d2_mixed,
+    c_diss,
+    c_advec_up,
+    c_advec_down,
+    NUM_DERIVATIVES_VARS
+};
 
 template <class deriv_t> class DerivativeTestsCompute
 {
-  protected:
-    const deriv_t m_deriv;
+  private:
+    deriv_t m_deriv;
 
   public:
-    DerivativeTestsCompute(double dx) : m_deriv(dx) {}
+
+    AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE DerivativeTestsCompute(double dx)
+        : m_deriv(dx)
+    {
+    }
 
     template <class data_t> struct Vars
     {
@@ -39,29 +55,34 @@ template <class deriv_t> class DerivativeTestsCompute
         }
     };
 
-    template <class data_t> void compute(Cell<data_t> current_cell) const
+    template <class data_t>
+    AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void
+    operator()(int i, int j, int k, const amrex::Array4<data_t> &out,
+               const amrex::Array4<data_t const> &in) const
     {
-        const auto out_d1 = m_deriv.template diff1<Vars>(current_cell);
-        const auto out_d2 = m_deriv.template diff2<Vars>(current_cell);
+        const auto out_d1 = m_deriv.template diff1<Vars>(i, j, k, in);
+        const auto out_d2 = m_deriv.template diff2<Vars>(i, j, k, in);
 
         Vars<data_t> out_diss;
         VarsTools::assign(out_diss, 0.);
-        m_deriv.add_dissipation(out_diss, current_cell, 1.0);
+        m_deriv.add_dissipation(i, j, k, out_diss, in, 1.0);
 
         Tensor<1, data_t> shift_down = {-2., 0., -3.};
         const auto out_advec_down =
-            m_deriv.template advection<Vars>(current_cell, shift_down);
+            m_deriv.template advection<Vars>(i, j, k, in, shift_down);
 
         Tensor<1, data_t> shift_up = {2., 0., 3.};
         const auto out_advec_up =
-            m_deriv.template advection<Vars>(current_cell, shift_up);
+            m_deriv.template advection<Vars>(i, j, k, in, shift_up);
 
-        current_cell.store_vars(out_d1.d1[2], c_d1);
-        current_cell.store_vars(out_d2.d2[2][2], c_d2);
-        current_cell.store_vars(out_d2.d2[0][2], c_d2_mixed);
-        current_cell.store_vars(out_diss.diss, c_diss);
-        current_cell.store_vars(out_advec_down.advec_down, c_advec_down);
-        current_cell.store_vars(out_advec_up.advec_up, c_advec_up);
+        const auto out_cell_data = out.cellData(i, j, k);
+
+        out_cell_data[c_d1]         = out_d1.d1[2];
+        out_cell_data[c_d2]         = out_d2.d2[2][2];
+        out_cell_data[c_d2_mixed]   = out_d2.d2[0][2];
+        out_cell_data[c_diss]       = out_diss.diss;
+        out_cell_data[c_advec_down] = out_advec_down.advec_down;
+        out_cell_data[c_advec_up]   = out_advec_up.advec_up;
     }
 };
 
