72219349a8723d539f40810aa32aaff12819a948
Miren Radia <miren_radia@yahoo.co.uk>
2023-06-06 15:48:11 +0100

    Add Coordinate Transformations test case

--- Tests/CoordinateTransformationsTest/CoordinateTransformationsTest.cpp
+++ Tests/CoordinateTransformationsTest/CoordinateTransformationsTest.cpp
@@ -3,84 +3,68 @@
  * Please refer to LICENSE in GRChombo's root directory.
  */
 
-// Chombo includes
-#include "IntVect.H"
+// Catch2 header
+#include "catch_amalgamated.hpp"
+
+// AMReX includes
+#include "AMReX.H"
+#include "AMReX_IntVect.H"
 
 // Other includes
+#include <limits>
+
+// Our includes
 #include "CoordinateTransformations.hpp"
 #include "Coordinates.hpp"
 #include "TensorAlgebra.hpp"
-#include "simd.hpp"
-#include <limits>
-
-// Chombo namespace
-#include "UsingNamespace.H"
-
-constexpr int ulp = 15; /* units in the last place */
-
-bool almost_equal(double value, double correct_value,
-                  int a_ulp /* units in the last place */)
+// #include "simd.hpp"
+namespace
 {
-    double diff    = std::abs(value - correct_value);
-    double epsilon = std::numeric_limits<double>::epsilon();
-    return (diff <
-            std::max(epsilon * std::abs(correct_value + value), epsilon) *
-                a_ulp);
-}
+constexpr int ulp               = 15; // units in the last place
+constexpr double double_epsilon = std::numeric_limits<double>::epsilon();
 
-bool check_tensor(const Tensor<2, double> &tensor,
+void check_tensor(const Tensor<2, double> &tensor,
                   const Tensor<2, double> &correct_tensor,
                   const std::string &test_name)
 {
-    bool failed = false;
     FOR (i, j)
     {
-        if (!almost_equal(tensor[i][j], correct_tensor[i][j], ulp))
-        {
-            std::cout << "Failed " << test_name << " in component [" << i
-                      << "][" << j << "]\n";
-            std::cout << "value: " << tensor[i][j] << "\n";
-            std::cout << "correct_value: " << correct_tensor[i][j] << std::endl;
-            failed = true;
-        }
+        INFO(test_name << ": component [" << i << "][" << j << "]");
+        CHECK_THAT(tensor[i][j],
+                   Catch::Matchers::WithinAbs(correct_tensor[i][j],
+                                              ulp * double_epsilon) ||
+                       Catch::Matchers::WithinRel(correct_tensor[i][j],
+                                                  ulp * double_epsilon));
     }
-    return failed;
 }
 
-bool check_vector(const Tensor<1, double> &vector,
+void check_vector(const Tensor<1, double> &vector,
                   const Tensor<1, double> &correct_vector,
                   const std::string &test_name)
 {
-    bool failed = false;
     FOR (i)
     {
-        if (!almost_equal(vector[i], correct_vector[i], ulp))
-        {
-            std::cout << "Failed " << test_name << " in component [" << i
-                      << "]\n";
-            std::cout << "value: " << vector[i] << "\n";
-            std::cout << "correct_value: " << correct_vector[i] << std::endl;
-            failed = true;
-        }
+        INFO(test_name << ": component [" << i << "]");
+        CHECK_THAT(vector[i], Catch::Matchers::WithinAbs(
+                                  correct_vector[i], ulp * double_epsilon) ||
+                                  Catch::Matchers::WithinRel(
+                                      correct_vector[i], ulp * double_epsilon));
     }
-    return failed;
 }
+} // namespace
 
-int main()
+TEST_CASE("Coordinate Transformations")
 {
-    bool failed     = false;
+    amrex::Initialize(MPI_COMM_WORLD);
     const double dx = 0.1;
-    IntVect iv;
-    iv[0] = 0;
-    iv[1] = 0;
-    iv[2] = 0;
+    amrex::IntVect iv{1, 2, 3};
 
     Coordinates<double> coords(iv, dx);
     const double x     = coords.x;
     const double y     = coords.y;
     const double z     = coords.z;
     const double r     = coords.get_radius();
-    double rho2        = simd_max(x * x + y * y, 1e-12);
+    double rho2        = std::max(x * x + y * y, 1e-12);
     double r2sin2theta = rho2;
 
     /* for debugging
@@ -97,7 +81,7 @@ int main()
     Tensor<2, double> jac           = spherical_jacobian(x, y, z);
     Tensor<2, double> inv_jac       = inverse_spherical_jacobian(x, y, z);
     Tensor<2, double> inv_jac_check = compute_inverse(jac);
-    failed |= check_tensor(inv_jac, inv_jac_check, "inverse_jacobian");
+    check_tensor(inv_jac, inv_jac_check, "inverse_jacobian");
 
     // Test tensor transformations
     Tensor<2, double> Mij_cart;
@@ -121,32 +105,28 @@ int main()
     // Test cartesian_to_spherical_LL
     Tensor<2, double> Mij_spher_check;
     Mij_spher_check = cartesian_to_spherical_LL(Mij_cart, x, y, z);
-    failed |=
-        check_tensor(Mij_spher_check, Mij_spher, "cartesian_to_spherical_LL");
+    check_tensor(Mij_spher_check, Mij_spher, "cartesian_to_spherical_LL");
 
     // Test spherical_to_cartesian_LL
     Tensor<2, double> Mij_cart_check;
     Mij_cart_check = spherical_to_cartesian_LL(Mij_spher, x, y, z);
-    failed |=
-        check_tensor(Mij_cart_check, Mij_cart, "spherical_to_cartesian_LL");
+    check_tensor(Mij_cart_check, Mij_cart, "spherical_to_cartesian_LL");
 
     // Test cartesian_to_spherical_UU
     Tensor<2, double> Mij_spher_UU;
     Tensor<2, double> Mij_spher_UU_check;
     Mij_spher_UU_check =
         cartesian_to_spherical_UU(compute_inverse_sym(Mij_cart), x, y, z);
-    Mij_spher_UU  = compute_inverse_sym(Mij_spher);
-    failed       |= check_tensor(Mij_spher_UU_check, Mij_spher_UU,
-                                 "cartesian_to_spherical_UU");
+    Mij_spher_UU = compute_inverse_sym(Mij_spher);
+    check_tensor(Mij_spher_UU_check, Mij_spher_UU, "cartesian_to_spherical_UU");
 
     // Test spherical_to_cartesian_UU
     Tensor<2, double> Mij_cart_UU;
     Tensor<2, double> Mij_cart_UU_check;
     Mij_cart_UU_check =
         spherical_to_cartesian_UU(compute_inverse_sym(Mij_spher), x, y, z);
-    Mij_cart_UU  = compute_inverse_sym(Mij_cart);
-    failed      |= check_tensor(Mij_cart_UU_check, Mij_cart_UU,
-                                "spherical_to_cartesian_UU");
+    Mij_cart_UU = compute_inverse_sym(Mij_cart);
+    check_tensor(Mij_cart_UU_check, Mij_cart_UU, "spherical_to_cartesian_UU");
 
     // Test vector transformations
     Tensor<1, double> si_cart;
@@ -162,46 +142,30 @@ int main()
     // Test cartesian_to_spherical_U
     Tensor<1, double> si_spher_U_check;
     si_spher_U_check = cartesian_to_spherical_U(si_cart, x, y, z);
-    failed |=
-        check_vector(si_spher_U_check, si_spher, "cartesian_to_spherical_U");
+    check_vector(si_spher_U_check, si_spher, "cartesian_to_spherical_U");
 
     // Test spherical_to_cartesian_U
     Tensor<1, double> si_cart_U_check;
     si_cart_U_check = spherical_to_cartesian_U(si_spher, x, y, z);
-    failed |=
-        check_vector(si_cart_U_check, si_cart, "spherical_to_cartesian_U");
+    check_vector(si_cart_U_check, si_cart, "spherical_to_cartesian_U");
 
     // Test cartesian_to_spherical_L
     Tensor<1, double> si_spher_L_check;
     si_spher_L_check = cartesian_to_spherical_L(si_cart, x, y, z);
-    failed |=
-        check_vector(si_spher_L_check, si_spher, "cartesian_to_spherical_L");
+    check_vector(si_spher_L_check, si_spher, "cartesian_to_spherical_L");
 
     // Test spherical_to_cartesian_L
     Tensor<1, double> si_cart_L_check;
     si_cart_L_check = spherical_to_cartesian_L(si_spher, x, y, z);
-    failed |=
-        check_vector(si_cart_L_check, si_cart, "spherical_to_cartesian_L");
+    check_vector(si_cart_L_check, si_cart, "spherical_to_cartesian_L");
 
     // Test area_element_sphere
     double area_element       = r * sqrt(rho2);
     double area_element_check = area_element_sphere(Mij_spher);
-    if (!almost_equal(area_element, area_element_check, ulp))
-    {
-        std::cout << "Failed area_element\n";
-        std::cout << "value: " << area_element << "\n";
-        std::cout << "correct_value: " << area_element_check << std::endl;
-        failed = true;
-    }
+    CHECK_THAT(area_element, Catch::Matchers::WithinAbs(area_element_check,
+                                                        ulp * double_epsilon) ||
+                                 Catch::Matchers::WithinRel(
+                                     area_element_check, ulp * double_epsilon));
 
-    if (failed)
-    {
-        std::cout << "Coordinate transformations test failed..." << std::endl;
-        return failed;
-    }
-    else
-    {
-        std::cout << "Coordinate transformations test passed..." << std::endl;
-        return 0;
-    }
+    amrex::Finalize();
 }
