9ed6723834024ab63134bd758135cf23e457147a
Miren Radia <miren_radia@yahoo.co.uk>
2023-06-14 17:11:28 +0100

    Tests: Make Catch2 play nicer with MPI
    Initializing and Finalizing MPI multiple times is erroneous so do it
    outside of AMReX's initialization and finalization.
    
    Only print Catch::cout() output on rank 0.

---
+++ Tests/Common/CatchOutput.cpp
@@ -0,0 +1,46 @@
+// Catch2 header
+#include "catch_amalgamated.hpp"
+
+// AMReX includes
+#include "AMReX_ccse-mpi.H"
+
+// System includes
+#include <fstream>
+#include <iostream>
+
+#ifdef BL_USE_MPI
+namespace Catch
+{
+// Catch2 doesn't really know anything about MPI so override the Catch::cout()
+// function here so that only rank 0 prints to stdout and the output is not
+// garbled.
+std::ostream &hide_output_from_non_zero_ranks(std::ostream &a_rank_zero_ostream)
+{
+    int mpi_initialized = false;
+    MPI_Initialized(&mpi_initialized);
+    if (!mpi_initialized)
+    {
+        return a_rank_zero_ostream;
+    }
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0)
+    {
+        return a_rank_zero_ostream;
+    }
+    static std::ofstream null_stream;
+    if (!null_stream.is_open())
+    {
+        null_stream.open("/dev/null", std::ofstream::out | std::ofstream::app);
+    }
+    return null_stream;
+}
+
+std::ostream &cout() { return hide_output_from_non_zero_ranks(std::cout); }
+
+std::ostream &cerr() { return std::cerr; }
+
+std::ostream &clog() { return std::clog; }
+
+} // namespace Catch
+#endif
--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -3,14 +3,24 @@
 #include "catch_amalgamated.hpp"
 
 #include "AMReX_REAL.H"
+#include "AMReX_ccse-mpi.H"
 
 int main(int argc, char *argv[])
 {
+#ifdef BL_USE_MPI
+    // We can only initialize and finalize MPI once so do it here
+    MPI_Init(&argc, &argv);
+#endif
+
     constexpr int cout_precision          = 17;
     Catch::StringMaker<double>::precision = cout_precision;
     Catch::StringMaker<float>::precision  = cout_precision;
 
     int result = Catch::Session().run(argc, argv);
+
+#ifdef BL_USE_MPI
+    MPI_Finalize();
+#endif
 
     return result;
 }
