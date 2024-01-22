5fe898b73e58777a20c14873fb6b426b052254cc
Miren Radia <miren_radia@yahoo.co.uk>
2024-01-22 15:36:37 +0000

    Tests: Add Weyl4 test that compares with GRChombo
    This test writes the computed values to an HDF5 file which is then
    compared with the GRChombo equivalent using h5diff. Note that this test
    is skipped if built without HDF5.

--- Tests/TestCases.hpp
+++ Tests/TestCases.hpp
@@ -8,6 +8,9 @@
 // doctest header
 #include "doctest.h"
 
+// AMReX includes
+#include <AMReX.H>
+
 // Test cases
 #include "CCZ4GeometryUnitTest.hpp"
 #include "CCZ4RHSTest.hpp"
@@ -15,6 +18,7 @@
 #include "DerivativeUnitTests.hpp"
 #include "PositiveChiAndAlphaUnitTest.hpp"
 #include "SphericalHarmonicTest.hpp"
+#include "Weyl4Test.hpp"
 
 TEST_CASE("CCZ4 Geometry") { run_ccz4_geometry_unit_tests(); }
 
@@ -30,5 +34,14 @@ TEST_CASE("Derivative Unit Tests") { run_derivative_unit_tests(); }
 TEST_CASE("Positive Chi and Alpha") { run_positive_chi_and_alpha_unit_test(); }
 
 TEST_CASE("Spherical Harmonics") { run_spherical_harmonic_test(); }
+
+TEST_CASE("Weyl4"
+#ifndef AMREX_USE_HDF5
+          * doctest::skip()
+#endif
+)
+{
+    run_weyl4_test();
+}
 
 #endif /* TESTCASES_HPP_ */
