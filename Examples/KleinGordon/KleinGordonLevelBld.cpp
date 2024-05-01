f0e1e9b739c95a17bad535bd2b7af1f283f3b1e3
julianakwan <julianakwan123@gmail.com>
2024-05-01 13:46:30 +0100

    apply clang formatting

--- Examples/KleinGordon/KleinGordonLevelBld.cpp
+++ Examples/KleinGordon/KleinGordonLevelBld.cpp
@@ -3,47 +3,34 @@
 
 using namespace amrex;
 
-class KleinGordonLevelBld
-    : public LevelBld
+class KleinGordonLevelBld : public LevelBld
 {
-    virtual void variableSetUp () override;
-    virtual void variableCleanUp () override;
-    virtual AmrLevel *operator() () override;
-    virtual AmrLevel *operator() (Amr& amr, int lev, const Geometry& gm,
-                                  const BoxArray& ba, const DistributionMapping& dm,
-                                  Real time) override;
+    virtual void variableSetUp() override;
+    virtual void variableCleanUp() override;
+    virtual AmrLevel *operator()() override;
+    virtual AmrLevel *operator()(Amr &amr, int lev, const Geometry &gm,
+                                 const BoxArray &ba,
+                                 const DistributionMapping &dm,
+                                 Real time) override;
 };
 
 KleinGordonLevelBld KleinGordon_bld;
 
-LevelBld*
-getLevelBld ()
-{
-    return &KleinGordon_bld;
-}
+LevelBld *getLevelBld() { return &KleinGordon_bld; }
 
-void
-KleinGordonLevelBld::variableSetUp ()
-{
-    KleinGordonLevel::variableSetUp();
-}
+void KleinGordonLevelBld::variableSetUp() { KleinGordonLevel::variableSetUp(); }
 
-void
-KleinGordonLevelBld::variableCleanUp ()
+void KleinGordonLevelBld::variableCleanUp()
 {
     KleinGordonLevel::variableCleanUp();
 }
 
-AmrLevel*
-KleinGordonLevelBld::operator() ()
-{
-    return new KleinGordonLevel;
-}
+AmrLevel *KleinGordonLevelBld::operator()() { return new KleinGordonLevel; }
 
-AmrLevel*
-KleinGordonLevelBld::operator() (Amr& amr, int lev, const Geometry& gm,
-                          const BoxArray& ba, const DistributionMapping& dm,
-                          Real time)
+AmrLevel *KleinGordonLevelBld::operator()(Amr &amr, int lev, const Geometry &gm,
+                                          const BoxArray &ba,
+                                          const DistributionMapping &dm,
+                                          Real time)
 {
     return new KleinGordonLevel(amr, lev, gm, ba, dm, time);
 }
--- Examples/KleinGordon/UserVariables.hpp
+++ Examples/KleinGordon/UserVariables.hpp
@@ -10,23 +10,22 @@
 #include "CCZ4UserVariables.hpp"
 #include "DiagnosticVariables.hpp"
 
-// assign an enum to each variable                                                                   
+// assign an enum to each variable
 enum
 {
-    // Note that it is important that the first enum value is set to 1 more than                     
+    // Note that it is important that the first enum value is set to 1 more than
 
-    c_phi, 
-    c_Pi,   
+    c_phi,
+    c_Pi,
 
     NUM_VARS,
 };
 
 namespace UserVariables
 {
-static const std::array<std::string, NUM_VARS>
-    variable_names = {"phi", "Pi"};
+static const std::array<std::string, NUM_VARS> variable_names = {"phi", "Pi"};
 
-} // namespace UserVariables     
+} // namespace UserVariables
 #include "UserVariables.inc.hpp"
 
 #endif /* USERVARIABLES_HPP */
