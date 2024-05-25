9c8cbf5a3d42132ed7011cd572d3050e4c71ece9
julianakwan <julianakwan123@gmail.com>
2024-05-25 19:41:38 +0100

    remove GRChombo references

--- Examples/ScalarField/ScalarFieldLevel.cpp
+++ Examples/ScalarField/ScalarFieldLevel.cpp
@@ -5,115 +5,111 @@
 
 // General includes common to most GR problems
 #include "ScalarFieldLevel.hpp"
-#include "BoxLoops.hpp"
-#include "NanCheck.hpp"
-#include "PositiveChiAndAlpha.hpp"
-#include "SixthOrderDerivatives.hpp"
-#include "TraceARemoval.hpp"
+// #include "PositiveChiAndAlpha.hpp"
+// #include "TraceARemoval.hpp"
+// //#include "SixthOrderDerivatives.hpp"
 
-// For RHS update
-#include "MatterCCZ4RHS.hpp"
+// // For RHS update
+// #include "MatterCCZ4RHS.hpp"
 
-// For constraints calculation
-#include "NewMatterConstraints.hpp"
+// // For constraints calculation
+// #include "NewMatterConstraints.hpp"
 
-// For tag cells
-#include "FixedGridsTaggingCriterion.hpp"
-
-// Problem specific includes
-#include "ComputePack.hpp"
-#include "GammaCalculator.hpp"
-#include "InitialScalarData.hpp"
-#include "KerrBH.hpp"
-#include "Potential.hpp"
-#include "ScalarField.hpp"
-#include "SetValue.hpp"
+// // Problem specific includes
+// //#include "ComputePack.hpp"
+// //#include "GammaCalculator.hpp"
+// #include "InitialScalarData.hpp"
+// #include "Potential.hpp"
+// #include "ScalarField.hpp"
+// //#include "SetValue.hpp"
 
 // Things to do at each advance step, after the RK4 is calculated
 void ScalarFieldLevel::specificAdvance()
 {
-    // Enforce trace free A_ij and positive chi and alpha
-    BoxLoops::loop(
-        make_compute_pack(TraceARemoval(),
-                          PositiveChiAndAlpha(m_p.min_chi, m_p.min_lapse)),
-        m_state_new, m_state_new, INCLUDE_GHOST_CELLS);
+    // // Enforce trace free A_ij and positive chi and alpha
+    // BoxLoops::loop(
+    //     make_compute_pack(TraceARemoval(),
+    //                       PositiveChiAndAlpha(m_p.min_chi, m_p.min_lapse)),
+    //     m_state_new, m_state_new, INCLUDE_GHOST_CELLS);
 
-    // Check for nan's
-    if (m_p.nan_check)
-        BoxLoops::loop(NanCheck(), m_state_new, m_state_new,
-                       EXCLUDE_GHOST_CELLS, disable_simd());
+    // // Check for nan's
+    // if (m_p.nan_check)
+    //     BoxLoops::loop(NanCheck(), m_state_new, m_state_new,
+    //                    EXCLUDE_GHOST_CELLS, disable_simd());
 }
 
 // Initial data for field and metric variables
 void ScalarFieldLevel::initialData()
 {
-    BL_PROFILE("ScalarFieldLevel::initialData");
-    if (m_verbosity)
-        amrex::Print() << "ScalarFieldLevel::initialData " << m_level << endl;
+    // BL_PROFILE("ScalarFieldLevel::initialData");
+    // if (m_verbosity)
+    //     amrex::Print() << "ScalarFieldLevel::initialData " << m_level <<
+    //     endl;
 
-    // First set everything to zero then initial conditions for scalar field -
-    // here a Kerr BH and a scalar field profile
-    BoxLoops::loop(
-        make_compute_pack(SetValue(0.), KerrBH(m_p.kerr_params, m_dx),
-                          InitialScalarData(m_p.initial_params, m_dx)),
-        m_state_new, m_state_new, INCLUDE_GHOST_CELLS);
+    // // First set everything to zero then initial conditions for scalar field
+    // -
+    // // here a Kerr BH and a scalar field profile
+    // BoxLoops::loop(
+    //     make_compute_pack(SetValue(0.), KerrBH(m_p.kerr_params, m_dx),
+    //                       InitialScalarData(m_p.initial_params, m_dx)),
+    //     m_state_new, m_state_new, INCLUDE_GHOST_CELLS);
 
-    fillAllGhosts();
-    BoxLoops::loop(GammaCalculator(m_dx), m_state_new, m_state_new,
-                   EXCLUDE_GHOST_CELLS);
+    // fillAllGhosts();
+    // BoxLoops::loop(GammaCalculator(m_dx), m_state_new, m_state_new,
+    //                EXCLUDE_GHOST_CELLS);
 }
 
 #ifdef AMREX_USE_HDF5
 // Things to do before outputting a checkpoint file
 void ScalarFieldLevel::prePlotLevel()
 {
-    fillAllGhosts();
-    Potential potential(m_p.potential_params);
-    ScalarFieldWithPotential scalar_field(potential);
-    BoxLoops::loop(
-        MatterConstraints<ScalarFieldWithPotential>(
-            scalar_field, m_dx, m_p.G_Newton, c_Ham, Interval(c_Mom, c_Mom)),
-        m_state_new, m_state_diagnostics, EXCLUDE_GHOST_CELLS);
+    // fillAllGhosts();
+    // Potential potential(m_p.potential_params);
+    // ScalarFieldWithPotential scalar_field(potential);
+    // BoxLoops::loop(
+    //     MatterConstraints<ScalarFieldWithPotential>(
+    //         scalar_field, m_dx, m_p.G_Newton, c_Ham, Interval(c_Mom, c_Mom)),
+    //     m_state_new, m_state_diagnostics, EXCLUDE_GHOST_CELLS);
 }
 #endif
 
 // Things to do in RHS update, at each RK4 step
-void ScalarFieldLevel::specificEvalRHS(GRLevelData &a_soln, GRLevelData &a_rhs,
+void ScalarFieldLevel::specificEvalRHS(amrex::MultiFab &a_soln,
+                                       amrex::MultiFab &a_rhs,
                                        const double a_time)
 {
-    // Enforce trace free A_ij and positive chi and alpha
-    BoxLoops::loop(
-        make_compute_pack(TraceARemoval(),
-                          PositiveChiAndAlpha(m_p.min_chi, m_p.min_lapse)),
-        a_soln, a_soln, INCLUDE_GHOST_CELLS);
+    // // Enforce trace free A_ij and positive chi and alpha
+    // BoxLoops::loop(
+    //     make_compute_pack(TraceARemoval(),
+    //                       PositiveChiAndAlpha(m_p.min_chi, m_p.min_lapse)),
+    //     a_soln, a_soln, INCLUDE_GHOST_CELLS);
 
-    // Calculate MatterCCZ4 right hand side with matter_t = ScalarField
-    Potential potential(m_p.potential_params);
-    ScalarFieldWithPotential scalar_field(potential);
-    if (m_p.max_spatial_derivative_order == 4)
-    {
-        MatterCCZ4RHS<ScalarFieldWithPotential, MovingPunctureGauge,
-                      FourthOrderDerivatives>
-            my_ccz4_matter(scalar_field, m_p.ccz4_params, m_dx, m_p.sigma,
-                           m_p.formulation, m_p.G_Newton);
-        BoxLoops::loop(my_ccz4_matter, a_soln, a_rhs, EXCLUDE_GHOST_CELLS);
-    }
-    else if (m_p.max_spatial_derivative_order == 6)
-    {
-        MatterCCZ4RHS<ScalarFieldWithPotential, MovingPunctureGauge,
-                      SixthOrderDerivatives>
-            my_ccz4_matter(scalar_field, m_p.ccz4_params, m_dx, m_p.sigma,
-                           m_p.formulation, m_p.G_Newton);
-        BoxLoops::loop(my_ccz4_matter, a_soln, a_rhs, EXCLUDE_GHOST_CELLS);
-    }
+    // // Calculate MatterCCZ4 right hand side with matter_t = ScalarField
+    // Potential potential(m_p.potential_params);
+    // ScalarFieldWithPotential scalar_field(potential);
+    // if (m_p.max_spatial_derivative_order == 4)
+    // {
+    //     MatterCCZ4RHS<ScalarFieldWithPotential, MovingPunctureGauge,
+    //                   FourthOrderDerivatives>
+    //         my_ccz4_matter(scalar_field, m_p.ccz4_params, m_dx, m_p.sigma,
+    //                        m_p.formulation, m_p.G_Newton);
+    //     BoxLoops::loop(my_ccz4_matter, a_soln, a_rhs, EXCLUDE_GHOST_CELLS);
+    // }
+    // else if (m_p.max_spatial_derivative_order == 6)
+    // {
+    //     MatterCCZ4RHS<ScalarFieldWithPotential, MovingPunctureGauge,
+    //                   SixthOrderDerivatives>
+    //         my_ccz4_matter(scalar_field, m_p.ccz4_params, m_dx, m_p.sigma,
+    //                        m_p.formulation, m_p.G_Newton);
+    //     BoxLoops::loop(my_ccz4_matter, a_soln, a_rhs, EXCLUDE_GHOST_CELLS);
+    // }
 }
 
 // Things to do at ODE update, after soln + rhs
-void ScalarFieldLevel::specificUpdateODE(GRLevelData &a_soln,
-                                         const GRLevelData &a_rhs, Real a_dt)
+void ScalarFieldLevel::specificUpdateODE(amrex::MultiFab &a_soln)
 {
-    // Enforce trace free A_ij
-    BoxLoops::loop(TraceARemoval(), a_soln, a_soln, INCLUDE_GHOST_CELLS);
+    // // Enforce trace free A_ij
+    // BoxLoops::loop(TraceARemoval(), a_soln, a_soln, INCLUDE_GHOST_CELLS);
 }
 
 void ScalarFieldLevel::preTagCells()
@@ -122,10 +118,12 @@ void ScalarFieldLevel::preTagCells()
     // used here so don't fill any
 }
 
-void ScalarFieldLevel::computeTaggingCriterion(FArrayBox &tagging_criterion,
-                                               const FArrayBox &current_state)
+void ScalarFieldLevel::computeTaggingCriterion(
+    amrex::TagBoxArray &tagging_criterion, int clearval, int tagval,
+    amrex::Real time, int n_error_buf = 0, int ngrow = 0)
+
 {
-    BoxLoops::loop(
-        FixedGridsTaggingCriterion(m_dx, m_level, 2.0 * m_p.L, m_p.center),
-        current_state, tagging_criterion);
+    // BoxLoops::loop(
+    //     FixedGridsTaggingCriterion(m_dx, m_level, 2.0 * m_p.L, m_p.center),
+    //     current_state, tagging_criterion);
 }
--- Examples/ScalarField/SimulationParameters.hpp
+++ Examples/ScalarField/SimulationParameters.hpp
@@ -12,7 +12,6 @@
 
 // Problem specific includes:
 #include "InitialScalarData.hpp"
-#include "KerrBH.hpp"
 #include "Potential.hpp"
 
 class SimulationParameters : public SimulationParametersBase
@@ -35,11 +34,6 @@ class SimulationParameters : public SimulationParametersBase
         pp.load("scalar_amplitude", initial_params.amplitude, 0.1);
         pp.load("scalar_width", initial_params.width, 1.0);
         pp.load("scalar_mass", potential_params.scalar_mass, 0.1);
-
-        // Initial Kerr data
-        pp.load("kerr_mass", kerr_params.mass, 1.0);
-        pp.load("kerr_spin", kerr_params.spin, 0.0);
-        pp.load("kerr_center", kerr_params.center, center);
     }
 
     void check_params()
@@ -52,28 +46,12 @@ class SimulationParameters : public SimulationParametersBase
         warn_parameter("scalar_width", initial_params.width,
                        initial_params.width < 0.5 * L,
                        "is greater than half the domain size");
-        warn_parameter("kerr_mass", kerr_params.mass, kerr_params.mass >= 0.0,
-                       "should be >= 0.0");
-        check_parameter("kerr_spin", kerr_params.spin,
-                        std::abs(kerr_params.spin) <= kerr_params.mass,
-                        "must satisfy |a| <= M = " +
-                            std::to_string(kerr_params.mass));
-        FOR (idir)
-        {
-            std::string name = "kerr_center[" + std::to_string(idir) + "]";
-            warn_parameter(
-                name, kerr_params.center[idir],
-                (kerr_params.center[idir] >= 0) &&
-                    (kerr_params.center[idir] <= (ivN[idir] + 1) * coarsest_dx),
-                "should be within the computational domain");
-        }
     }
 
     // Initial data for matter and potential and BH
     double G_Newton;
     InitialScalarData::params_t initial_params;
     Potential::params_t potential_params;
-    KerrBH::params_t kerr_params;
 };
 
 #endif /* SIMULATIONPARAMETERS_HPP_ */
