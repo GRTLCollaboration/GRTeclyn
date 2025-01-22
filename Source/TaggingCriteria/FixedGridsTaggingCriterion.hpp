aab46aac3a632f8ac27cc49e70fa427f5a5c1054
Juliana Kwan <jk945@cam.ac.uk>
2025-01-22 09:52:18 +0000

    Port fixed grids tagging (#85)
    * Tagging: Port fixed grid tagging
    
    Also change from chi tagging to fixed grid tagging in BinaryBH
    example
    
    BinaryBH: Remove fill patch
    
    Fill patch not needed for fixed grid tagging
    
    * Tagging: Address Linter report
    
    Also reverts back to original chi tagging criterion for BinaryBHLevel
    because I had changed it to fixed grid tagging for the weak scaling
    tests. I guess it should be chi tagging by default.
    
    * Tagging: Remove current cell from function signature
    
    As per Miren's review of PR#85, I've removed passing in the current cell
    to the compute function as it is not needed in the calculations.

--- Source/TaggingCriteria/FixedGridsTaggingCriterion.hpp
+++ Source/TaggingCriteria/FixedGridsTaggingCriterion.hpp
@@ -14,33 +14,37 @@
 class FixedGridsTaggingCriterion
 {
   protected:
-    const double m_dx;
-    const double m_L;
-    const int m_level;
-    const std::array<double, AMREX_SPACEDIM> m_center;
+    double m_dx;
+    double m_L;
+    int m_level;
+    std::array<double, AMREX_SPACEDIM> m_center;
 
   public:
+    // NOLINTBEGIN(bugprone-easily-swappable-parameters)
     FixedGridsTaggingCriterion(
         const double dx, const int a_level, const double a_L,
         const std::array<double, AMREX_SPACEDIM> a_center)
         : m_dx(dx), m_L(a_L), m_level(a_level), m_center(a_center){};
-
-    template <class data_t> void compute(Cell<data_t> current_cell) const
+    // NOLINTEND(bugprone-easily-swappable-parameters)
+    template <class data_t>
+    AMREX_GPU_DEVICE data_t compute(int i, int j, int k) const
     {
         data_t criterion = 0.0;
         // make sure the inner part is regridded around the horizon
         // take L as the length of full grid, so tag inner 1/2
         // of it, which means inner \pm L/4
         double ratio = pow(2.0, -(m_level + 2.0));
-        const Coordinates<data_t> coords(current_cell, m_dx, m_center);
+
+        amrex::IntVect cell(AMREX_D_DECL(i, j, k));
+
+        const Coordinates<data_t> coords(cell, m_dx, m_center);
         const data_t max_abs_xy =
             simd_max(std::abs(coords.x), std::abs(coords.y));
         const data_t max_abs_xyz = simd_max(max_abs_xy, std::abs(coords.z));
         auto regrid              = simd_compare_lt(max_abs_xyz, m_L * ratio);
         criterion                = simd_conditional(regrid, 100.0, criterion);
 
-        // Write back into the flattened Chombo box
-        current_cell.store_vars(criterion, 0);
+        return criterion;
     }
 };
 
