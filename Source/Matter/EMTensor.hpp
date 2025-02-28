c95f450420ef5132a4ffe06ba164c73e17704490
Juliana Kwan <julianakwan123@gmail.com>
2025-02-28 12:40:30 +0000

    Matter: Add new compute_mf feature consistent with AMReX
    This commit adds:
      - a set_up function to add the energy density, momemtum density
        and spatial stress-energy tensor as components of derive_lst
      - compute_mf a wrapper to the compute function which is called
        by AMReX whenever a derived variable needs to be calculated.

--- Source/Matter/EMTensor.hpp
+++ Source/Matter/EMTensor.hpp
@@ -24,6 +24,19 @@ template <class matter_t> class EMTensor
     template <class data_t>
     using Vars = typename MatterCCZ4RHS<matter_t>::template Vars<data_t>;
 
+    /// derive record name
+    static inline const std::string name = "EMTensor";
+
+    /// Variable names
+    static inline amrex::Vector<std::string> var_names = {"rho"};
+
+    /// 3 components for the momentum density: Si_11, Si_22, Si_33
+    /// 6 components for the stress energy tensor (symmetric): Sij_11, Sij_12
+    /// etc.
+    static inline const amrex::Vector<std::string> extra_var_names = {
+        "Sij_11, Sij_22, Sij_33, Sij_11, Sij_12, Sij_13, Sij_22, Sij_23, "
+        "Sij_33"};
+
     //! Constructor
     EMTensor(const matter_t a_matter, const double dx, const int a_c_rho = -1,
              const Interval a_c_Si  = Interval(),
@@ -34,11 +47,15 @@ template <class matter_t> class EMTensor
     compute(int i, int j, int k, const amrex::Array4<data_t> &out_mf,
             const amrex::Array4<const data_t> &in_mf) const;
 
-    static void compute_mf(amrex::MultiFab &out_mf, int dcomp, int ncomp,
-                           const amrex::MultiFab &src_mf,
-                           const amrex::Geometry &geomdata,
-                           amrex::Real /*time*/, const int * /*bcrec*/,
-                           int /*level*/);
+    // Set do_all_components to true to calculate the momentum density
+    // and stress energy tensors as well
+    AMREX_FORCE_INLINE static void set_up(int a_state_index,
+                                          bool do_all_components = false);
+
+    AMREX_FORCE_INLINE static void
+    compute_mf(amrex::MultiFab &out_mf, int dcomp, int ncomp,
+               const amrex::MultiFab &src_mf, const amrex::Geometry &geomdata,
+               amrex::Real /*time*/, const int * /*bcrec*/, int /*level*/);
 
   protected:
     const matter_t m_matter;
