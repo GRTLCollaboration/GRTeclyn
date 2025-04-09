5c96ab525847261736f5ff2e14ebac3338be668b
Juliana Kwan <jkwan@aurora-uan-0012.hostmgmt.cm.aurora.alcf.anl.gov>
2025-04-09 09:28:15 +0000

    Core: Change min to std::min for SYCL backend

--- Source/utils/Macros.hpp
+++ Source/utils/Macros.hpp
@@ -40,7 +40,7 @@ constexpr int GR_SPACEDIM = 3;
     GET_MACRO6(__VA_ARGS__, FOR5, FOR4, FOR3, FOR2, FOR1, DUMMYFOR)(__VA_ARGS__)\
 // NOLINTEND(cppcoreguidelines-macro-usage)
 
-#define SYMM_INDEX(IDX0, IDX1, IDX2) IDX1 + IDX2 + min(IDX1, IDX2) < 5 ? IDX0 + IDX1 + IDX2 + min(IDX1, IDX2) : IDX0 + 5
+#define SYMM_INDEX(IDX0, IDX1, IDX2) IDX1 + IDX2 + std::min(IDX1, IDX2) < 5 ? IDX0 + IDX1 + IDX2 + std::min(IDX1, IDX2) : IDX0 + 5
 #define INDEX(IDX0, IDX1, IDX2) IDX0 + (GR_SPACEDIM)*IDX1 + IDX2
 
 #endif /* GRUTILS_HPP_*/
