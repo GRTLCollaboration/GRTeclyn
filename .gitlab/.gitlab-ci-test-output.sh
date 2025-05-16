ef3cd3afbd315596674ab6d739001bbf6f61d3db
Juliana Kwan <julianakwan123@gmail.com>
2025-05-16 17:23:57 +0100

    CI: Refactor while loop and BINARY_DIR
    This will move the definition of BINARY_DIR to the before_script
    so there is no need for a dotenv artifact. Also I changed the
    ordering of the while loop so it is hopefully clearer when things
    go wrong with the filesystem.

--- .gitlab/.gitlab-ci-test-output.sh
+++ .gitlab/.gitlab-ci-test-output.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#$1 = ${STORE}
+#$1 = ${BINARY_DIR}
 #$2 = ${CI_PROJECT_DIR}
 
 count=0
@@ -10,8 +10,8 @@ while [ $count -le 9 ]; do
 		"$1"/particle_compare.exe --rel_tol 1e-10 --abs_tol 1e-10 plt00008/ "${HOME}"/"$2"/.github/workflows/data/plt00008_compare/ particles
 		break
 	else
-		sleep 30
 		((count++))
 		echo "Attempt number " $count " of 10"
+		sleep 30
 	fi
 done
