b8aa5b850e8f02c6204ba9e3fb2dd58adb141907
Juliana Kwan <julianakwan123@gmail.com>
2025-06-23 14:55:34 +0100

    CI: Add style check for tabs and whitespace

---
+++ .github/workflows/style/check_trailing_whitespaces.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+find . -type d \( -name .git \
+                  -o -path ./paper \
+                  -o -name build -o -name install \
+                  -o -name tmp_build_dir -o -name tmp_install_dir \
+               \) -prune -o \
+       -type f \( -name "*.f" -o -name "*.F" -o -name "*.f90" -o -name "*.F90" \
+                  -o -name "*.py" \
+                  -o -name "*.rst" \
+                  -o -name "*.sh" \
+                  -o -name "*.tex" \
+                  -o -name "*.txt" \
+                  -o -name "*.yml" \
+               \) \
+    -exec grep -Iq . {} \; \
+    -exec sed -i 's/[[:blank:]]\+$//g' {} +
+
+gitdiff=`git diff`
+
+if [ -z "$gitdiff" ]
+then
+    exit 0
+else
+    echo -e "\nTrailing whitespaces at the end of a line are not allowed. Changes suggested by"
+    echo -e "  ${0}\n"
+    git --no-pager diff
+    echo ""
+    exit 1
+fi
