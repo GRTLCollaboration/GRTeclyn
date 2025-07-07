f0b6892b5edaed9288a31ce45d4d95a58464aa30
Juliana Kwan <jk945@cam.ac.uk>
2025-07-07 10:59:42 +0100

    CI: Add style check for all files (#123)
    * CI: Add style check for tabs and whitespace
    
    * CI: Remove irrelevant directories
    
    Some AMReX directories are not relevant for us e.g. paper
    so don't check for these
    
    * CI: Remove trailing white space
    
    * CI: Change to push only, version bump
    
    Bump version of GitHub checkout from v3 to v4

---
+++ .github/workflows/style/check_tabs.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+find . -type d \( -name .git \
+                  -o -name build -o -name install \
+                  -o -name tmp_build_dir \
+               \) -prune -o \
+       -type f \( -name "*.f" -o -name "*.F" -o -name "*.f90" -o -name "*.F90" \
+                  -o -name "*.py" \
+                  -o -name "*.md" -o -name "*.rst" \
+                  -o -name "*.sh" \
+                  -o -name "*.tex" \
+                  -o -name "*.txt" \
+                  -o -name "*.yml" \
+               \) \
+    -exec grep -Iq . {} \; \
+    -exec sed -i 's/\t/\ \ \ \ /g' {} +
+
+gitdiff=`git diff`
+
+if [ -z "$gitdiff" ]
+then
+    exit 0
+else
+    echo -e "\nTabs are not allowed. Changes suggested by"
+    echo -e "  ${0}\n"
+    git --no-pager diff
+    echo ""
+    exit 1
+fi
