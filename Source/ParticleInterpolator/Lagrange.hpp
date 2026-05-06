cc59b0fa863ce93372a39fb27d69bfe03748d487
Adam Greenbank <adam.greenbank@stfc.ac.uk>
2026-05-06 14:09:28 +0000

    Switch to calling interpolation functions using lambda

--- Source/ParticleInterpolator/Lagrange.hpp
+++ Source/ParticleInterpolator/Lagrange.hpp
@@ -60,10 +60,97 @@ template <int N> class Lagrange
         base_idx = stencil[0];
     }
 
+    // Different cases for interpolation of derivatives
+    amrex::ParticleReal
+    interp_deriv_local(int comp,
+                       const amrex::Array4<amrex::Real const> data) const
+    {
+        amrex::ParticleReal val = amrex::ParticleReal(0.0);
+
+#if AMREX_SPACEDIM == 3
+        for (int kk = 0; kk < N; ++kk)
+        {
+#endif
+#if AMREX_SPACEDIM >= 2
+            for (int jj = 0; jj < N; ++jj)
+            {
+#endif
+                for (int ii = 0; ii < N; ++ii)
+                {
+                    val += data(amrex::IntVect(
+                                    AMREX_D_DECL(i0 + ii, j0 + jj, k0 + kk)),
+                                comp) *
+                           AMREX_D_TERM(weights[0][ii], *weights[1][jj],
+                                        *weights[2][kk]);
+                }
+#if AMREX_SPACEDIM >= 2
+            }
+#endif
+#if AMREX_SPACEDIM == 3
+        }
+#endif
+        return val;
+    }
+
+    amrex::ParticleReal
+    interp_deriv_1d(int comp, int dim, int stride,
+                    FourthOrderDerivatives a_deriv,
+                    const amrex::Array4<amrex::Real const> data) const
+    {
+        amrex::ParticleReal val = amrex::ParticleReal(0.0);
+
+        for (int ii = 0; ii < N; ++ii)
+        {
+            val += a_deriv.diff1(data.ptr(i0, j0, k0) +
+                                     comp * data.stride.a[2] + ii * stride,
+                                 stride) *
+                   weights[dim][ii];
+        }
+        return val;
+    }
+
+    amrex::ParticleReal
+    interp_deriv_2d(int comp, int dim, int stride,
+                    FourthOrderDerivatives a_deriv,
+                    const amrex::Array4<amrex::Real const> data) const
+    {
+        amrex::ParticleReal val = amrex::ParticleReal(0.0);
+
+        for (int ii = 0; ii < N; ++ii)
+        {
+            val += a_deriv.diff2(data.ptr(i0, j0, k0) +
+                                     comp * data.stride.a[2] + ii * stride,
+                                 stride) *
+                   weights[dim][ii];
+        }
+        return val;
+    }
+
+    amrex::ParticleReal
+    interp_deriv_2d_mixed(int comp, int dim1, int dim2, int stride1,
+                          int stride2, FourthOrderDerivatives a_deriv,
+                          const amrex::Array4<amrex::Real const> data) const
+    {
+        amrex::ParticleReal val = amrex::ParticleReal(0.0);
+
+        for (int ii = 0; ii < N; ++ii)
+        {
+            for (int jj = 0; jj < N; ++jj)
+            {
+                val += a_deriv.mixed_diff2(data.ptr(i0, j0, k0) +
+                                               comp * data.stride.a[2] +
+                                               ii * stride1 + jj * stride2,
+                                           stride1, stride2) *
+                       weights[dim1][ii] * weights[dim2][jj];
+            }
+        }
+        return val;
+    }
+
   public:
     // where we store the weights for each dimension
     // NOLINTBEGIN(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
-    amrex::Real weights[3][N]{};
+    amrex::Real weights[AMREX_SPACEDIM][N]{};
     // NOLINTEND(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
 
     AMREX_GPU_DEVICE AMREX_FORCE_INLINE Lagrange() = default;
@@ -116,109 +203,80 @@ template <int N> class Lagrange
 
             const Derivative &deriv = deriv_it->first;
 
+            amrex::GpuArray<int, AMREX_SPACEDIM> strides{1, data.stride.a[0],
+                                                         data.stride.a[1]};
+
+            FourthOrderDerivatives a_deriv(dx);
+
+            std::function<amrex::ParticleReal(int)> interp_deriv;
+
             comps_t &comps = deriv_it->second;
 
+            // Select which interpolation function to call based on derivative
+            if (deriv == Derivative::LOCAL)
+            {
+                interp_deriv = [=](int comp) -> amrex::ParticleReal
+                { return interp_deriv_local(comp, data); };
+            }
+            else
+            {
+                std::vector<int> deriv_dims;
+
+                // Collect dimensions where we want at least a first derivative
+                for (int dim = 0; dim < AMREX_SPACEDIM; ++dim)
+                {
+                    if (deriv[dim] > 0)
+                    {
+                        deriv_dims.push_back(dim);
+                    }
+                }
+
+                if (deriv_dims.size() == 1)
+                {
+                    // 2nd derivative in 1 dimension
+                    if (deriv[deriv_dims[0]] == 2)
+                    {
+                        interp_deriv = [=](int comp) -> amrex::ParticleReal
+                        {
+                            return interp_deriv_2d(comp, deriv_dims[0],
+                                                   strides[deriv_dims[0]],
+                                                   a_deriv, data);
+                        };
+                    }
+                    // 1st derivative
+                    else
+                    {
+                        interp_deriv = [=](int comp) -> amrex::ParticleReal
+                        {
+                            return interp_deriv_1d(comp, deriv_dims[0],
+                                                   strides[deriv_dims[0]],
+                                                   a_deriv, data);
+                        };
+                    }
+                }
+                // Mixed 2nd derivative
+                else if (deriv_dims.size() == 2)
+                {
+                    interp_deriv = [=](int comp) -> amrex::ParticleReal
+                    {
+                        return interp_deriv_2d_mixed(
+                            comp, deriv_dims[0], deriv_dims[1],
+                            strides[deriv_dims[0]], strides[deriv_dims[1]],
+                            a_deriv, data);
+                    };
+                }
+            }
+
+            // Interpolate for comps with this derivative
             for (auto &entry : comps)
             {
                 const int comp = entry.comp;
 
-                val[counter] = amrex::ParticleReal(0.0);
-
-                if (deriv == Derivative::LOCAL)
-                {
-
-#if AMREX_SPACEDIM == 3
-                    for (int kk = 0; kk < N; ++kk)
-                    {
-#endif
-#if AMREX_SPACEDIM >= 2
-                        for (int jj = 0; jj < N; ++jj)
-                        {
-#endif
-                            for (int ii = 0; ii < N; ++ii)
-                            {
-                                val[counter] +=
-                                    data(amrex::IntVect(AMREX_D_DECL(
-                                             i0 + ii, j0 + jj, k0 + kk)),
-                                         comp) *
-                                    AMREX_D_TERM(weights[0][ii],
-                                                 *weights[1][jj],
-                                                 *weights[2][kk]);
-                            }
-#if AMREX_SPACEDIM >= 2
-                        }
-#endif
-#if AMREX_SPACEDIM == 3
-                    }
-#endif
-                }
-                else
-                {
-                    FourthOrderDerivatives a_deriv(dx);
-
-                    amrex::GpuArray<int, AMREX_SPACEDIM> strides{
-                        1, data.stride.a[0], data.stride.a[1]};
-
-                    int stride_dims[] = {0, 0};
-                    int deriv_dim     = 0;
-
-                    for (int dim = 0; dim < AMREX_SPACEDIM; ++dim)
-                    {
-                        if (deriv[dim] == 2)
-                        {
-                            for (int ii = 0; ii < N; ++ii)
-                            {
-                                val[counter] +=
-                                    a_deriv.diff2(data.ptr(i0, j0, k0) +
-                                                      comp * data.stride.a[2] +
-                                                      ii * strides[dim],
-                                                  strides[dim]) *
-                                    weights[dim][ii];
-                            }
-
-                            break;
-                        }
-                        else if (deriv[dim] == 1)
-                        {
-                            stride_dims[deriv_dim++] = dim;
-                        }
-                    }
-
-                    if (deriv_dim == 1)
-                    {
-                        for (int ii = 0; ii < N; ++ii)
-                        {
-                            val[counter] +=
-                                a_deriv.diff1(data.ptr(i0, j0, k0) +
-                                                  comp * data.stride.a[2] +
-                                                  ii * strides[stride_dims[0]],
-                                              strides[stride_dims[0]]) *
-                                weights[stride_dims[0]][ii];
-                        }
-                    }
-                    else if (deriv_dim == 2)
-                    {
-                        for (int ii = 0; ii < N; ++ii)
-                        {
-                            for (int jj = 0; jj < N; ++jj)
-                            {
-                                val[counter] +=
-                                    a_deriv.mixed_diff2(
-                                        data.ptr(i0, j0, k0) +
-                                            comp * data.stride.a[2] +
-                                            ii * strides[stride_dims[0]] +
-                                            jj * strides[stride_dims[1]],
-                                        strides[stride_dims[0]],
-                                        strides[stride_dims[1]]) *
-                                    weights[stride_dims[0]][ii] *
-                                    weights[stride_dims[1]][jj];
-                            }
-                        }
-                    }
-                }
+                val[counter] = interp_deriv(comp);
             }
+
             ++counter;
-        } // end of for comp loop
+        }
     }
 };
 #endif /* LAGRANGE_HPP_ */
