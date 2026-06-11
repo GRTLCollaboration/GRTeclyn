f7336fe5c1689a30a99b43e6b2b46e90150ee02a
Tamara Evstafyeva <tevstafyeva@perimeterinstitute.ca>
2026-06-11 10:33:31 -0400

    Address linter issues and add some debugging lines

--- Examples/KleinGordon/KleinGordonLevel.cpp
+++ Examples/KleinGordon/KleinGordonLevel.cpp
@@ -89,6 +89,7 @@ void KleinGordonLevel::initData()
 
     // NB: the analytic solutions are defined in InitialConditions.cpp
     // The functions below are defined in DerivedVariables.cpp
+    // NOLINTBEGIN(bugprone-branch-clone)
     if (model == "Wave")
     {
         calc_analytic_mf_3d<Wave>(state_new, dcomp, geom, current_time);
@@ -101,6 +102,7 @@ void KleinGordonLevel::initData()
     {
         calc_analytic_mf_3d<SineGordon>(state_new, dcomp, geom, current_time);
     }
+    // NOLINTEND(bugprone-branch-clone)
 }
 
 // NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
