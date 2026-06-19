cb83cd75eb2272051bcb9f19a8b76f368db98d5a
Miren Radia <miren_radia@yahoo.co.uk>
2026-06-19 09:37:19 +0100

    BinaryBH: Fix missing amrex namespace with OMP

--- Examples/BinaryBH/BinaryBHLevel.cpp
+++ Examples/BinaryBH/BinaryBHLevel.cpp
@@ -91,7 +91,7 @@ void BinaryBHLevel::initData()
 #endif
 
 #ifdef AMREX_USE_OMP
-#pragma omp parallel if (Gpu::notInLaunchRegion())
+#pragma omp parallel if (amrex::Gpu::notInLaunchRegion())
 #endif
     for (amrex::MFIter mfi(state_new, amrex::TilingIfNotGPU()); mfi.isValid();
          ++mfi)
