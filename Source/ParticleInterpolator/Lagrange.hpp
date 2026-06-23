9bac0648cbf0f9548659607df57d509e17a10463
Adam Greenbank <adam.greenbank@stfc.ac.uk>
2026-06-23 16:06:22 +0000

    Change derivative interoplation to use smaller stencil Directly interpolates Lagrange weight function instead of using finite difference stencils

--- Source/ParticleInterpolator/Lagrange.hpp
+++ Source/ParticleInterpolator/Lagrange.hpp
@@ -110,14 +110,71 @@ template <int N> class Lagrange
         base_idx = stencil[0];
     }
 
+    AMREX_GPU_DEVICE AMREX_FORCE_INLINE void
+    build_stencil_d1(amrex::Real grid_pos, amrex::Real *weights)
+    {
+        std::array<int, N> stencil;
+        constexpr int center_offset =
+            N / 2; // offset based on the number of points we are using
+        int center = static_cast<int>(
+            amrex::Math::round(grid_pos)); // round the grid position to get the
+                                           // nearest cell center
+
+        // Fill in the stencil around the position of interpolation
+        for (int i = 0; i < N; ++i)
+        {
+            stencil[i] = center - center_offset + i;
+        }
+
+        // Compute the relative position from the interpolation point
+        // NOLINTBEGIN(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
+        amrex::Real rel_pos[N];
+        // NOLINTEND(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
+        for (int i = 0; i < N; ++i)
+        {
+            rel_pos[i] = static_cast<amrex::Real>(stencil[i]) - grid_pos;
+        }
+
+        // Compute Lagrange weights
+        poly_interp_coeff_d1(0.0, rel_pos, N, weights);
+    }
+
+    AMREX_GPU_DEVICE AMREX_FORCE_INLINE void
+    build_stencil_d2(amrex::Real grid_pos, amrex::Real *weights)
+    {
+        std::array<int, N> stencil;
+        constexpr int center_offset =
+            N / 2; // offset based on the number of points we are using
+        int center = static_cast<int>(
+            amrex::Math::round(grid_pos)); // round the grid position to get the
+                                           // nearest cell center
+
+        // Fill in the stencil around the position of interpolation
+        for (int i = 0; i < N; ++i)
+        {
+            stencil[i] = center - center_offset + i;
+        }
+
+        // Compute the relative position from the interpolation point
+        // NOLINTBEGIN(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
+        amrex::Real rel_pos[N];
+        // NOLINTEND(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
+        for (int i = 0; i < N; ++i)
+        {
+            rel_pos[i] = static_cast<amrex::Real>(stencil[i]) - grid_pos;
+        }
+
+        // Compute Lagrange weights
+        poly_interp_coeff_d2(0.0, rel_pos, N, weights);
+    }
+
     AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void
     poly_interp_coeff_d1(amrex::Real xInt, amrex::Real const *AMREX_RESTRICT x,
-                         int order,
-                         amrex::Real *AMREX_RESTRICT weights) noexcept
+                         int order, amrex::Real *AMREX_RESTRICT c) noexcept
     {
         for (int j = 0; j < order; ++j)
         {
-            auto den = amrex::Real(1.0);
+            amrex::Real den = amrex::Real(1.0);
             for (int i = 0; i < order; ++i)
             {
                 if (i != j)
@@ -126,15 +183,14 @@ template <int N> class Lagrange
                 }
             }
 
-            weights[j] = amrex::Real(0.0);
+            c[j] = amrex::Real(0.0);
 
             for (int k = 0; k < order; ++k)
             {
                 if (k == j)
-                {
                     continue;
-                }
-                auto num = amrex::Real(1.0);
+
+                amrex::Real num = amrex::Real(1.0);
                 for (int i = 0; i < order; ++i)
                 {
                     if (i != j && i != k)
@@ -143,21 +199,20 @@ template <int N> class Lagrange
                     }
                 }
 
-                weights[j] += num / den;
+                c[j] += num / den;
             }
         }
     }
 
     AMREX_GPU_HOST_DEVICE AMREX_FORCE_INLINE void
     poly_interp_coeff_d2(amrex::Real xInt, amrex::Real const *AMREX_RESTRICT x,
-                         int order,
-                         amrex::Real *AMREX_RESTRICT weights) noexcept
+                         int order, amrex::Real *AMREX_RESTRICT c) noexcept
     {
         for (int j = 0; j < order; ++j)
         {
-            weights[j] = amrex::Real(0.0);
+            c[j] = amrex::Real(0.0);
 
-            auto den = amrex::Real(1.0);
+            amrex::Real den = amrex::Real(1.0);
             for (int i = 0; i < order; ++i)
             {
                 if (i != j)
@@ -169,22 +224,16 @@ template <int N> class Lagrange
             for (int l = 0; l < order; ++l)
             {
                 if (l == j)
-                {
                     continue;
-                }
 
                 for (int k = 0; k < order; ++k)
                 {
                     if (k == j)
-                    {
                         continue;
-                    }
                     if (k == l)
-                    {
                         continue;
-                    }
 
-                    auto num = amrex::Real(1.0);
+                    amrex::Real num = amrex::Real(1.0);
 
                     for (int i = 0; i < order; ++i)
                     {
@@ -194,7 +243,7 @@ template <int N> class Lagrange
                         }
                     }
 
-                    weights[j] += num / den;
+                    c[j] += num / den;
                 }
             }
         }
@@ -304,6 +353,43 @@ template <int N> class Lagrange
             for (int j = 0; j < comp_counts[i]; ++j)
             {
                 const int comp = comps[j].comp;
+
+                val[counter] = amrex::ParticleReal(0.0);
+
+#if AMREX_SPACEDIM == 3
+                for (int kk = 0; kk < N; ++kk)
+                {
+#endif
+#if AMREX_SPACEDIM >= 2
+                    for (int jj = 0; jj < N; ++jj)
+                    {
+#endif
+                        for (int ii = 0; ii < N; ++ii)
+                        {
+                            val[counter] +=
+                                data(amrex::IntVect(AMREX_D_DECL(
+                                         i0 + ii, j0 + jj, k0 + kk)),
+                                     comp) *
+                                AMREX_D_TERM(weights[0][ii], *weights[1][jj],
+                                             *weights[2][kk]);
+                        }
+#if AMREX_SPACEDIM >= 2
+                    }
+#endif
+#if AMREX_SPACEDIM == 3
+                }
+                else
+                {
+                    for (int i = 0; i < N; i++)
+                    {
+                        weights[dim][i] = weights_local[dim][i];
+                    }
+                }
+            }
+
+            for (auto &entry : comps)
+            {
+                const int comp = entry.comp;
 
                 val[counter] = amrex::ParticleReal(0.0);
 
