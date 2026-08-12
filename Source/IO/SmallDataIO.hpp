9dcf1e07baf6c01143078d38547f49014f0c99d4
Miren Radia <miren_radia@yahoo.co.uk>
2026-08-12 17:36:01 +0100

    IO: Add required AMReX_REAL.H header to SmallDataIO

--- Source/IO/SmallDataIO.hpp
+++ Source/IO/SmallDataIO.hpp
@@ -16,6 +16,8 @@
 #include <string>
 #include <vector>
 
+#include <AMReX_REAL.H>
+
 //! A class for reading and writing small data to a file in ASCII format.
 /*!
     A class for reading and writing small data, usually 0D, 1D or 2D, in ASCII
